4d327871895aec9cf20254b431bfaabcbf978ef3
STAM <stam@epicm.org>
2015-04-03 10:54:55 +0300

    new wpf window

--- Sources/EMArcadeShell/UI/WpfIntro.xaml.cs
+++ Sources/EMArcadeShell/UI/WpfIntro.xaml.cs
@@ -1,27 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows; 
 
 namespace EMArcadeShell.UI
 {
 	/// <summary>
 	/// Interaction logic for Window1.xaml
 	/// </summary>
-	public partial class Window1 : Window
+	public partial class WpfIntro : Window
 	{
-		public Window1()
+		public WpfIntro()
 		{
 			InitializeComponent();
+		}
+
+		private void WpfIntro_Loaded(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				WpfMediaElement.Source = new Uri(Directory.GetCurrentDirectory()+ "\\MEDIA\\epicmorg_logo.mp4");
+				WpfMediaElement.Play();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+			}
+		}
+
+		private void MediaElement_Ended(object sender, RoutedEventArgs e)
+		{
+			NextStep();
+		}
+
+		private void NextStep()
+		{
+
+			WpfMediaElement.Source = null;
+			
+			var gamelist = new WpfGamelist();
+			gamelist.Show();
 		}
 	}
 }
