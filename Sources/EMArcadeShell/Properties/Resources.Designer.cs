7b4d438f0fcf81b993ff8557078a35115b8be235
STAM <stam@epicm.org>
2015-04-08 17:36:20 +0300

    update

--- Sources/EMArcadeShell/Properties/Resources.Designer.cs
+++ Sources/EMArcadeShell/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace EMArcadeShell.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon fav {
+            get {
+                object obj = ResourceManager.GetObject("fav", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
--- Sources/EMArcadeShell/UI/WpfGamelist.xaml.cs
+++ Sources/EMArcadeShell/UI/WpfGamelist.xaml.cs
@@ -1,107 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;  
 using System.IO;
-using System.Runtime.Remoting.Channels;
-using System.Threading;
 using System.Windows;
-using System.Windows.Controls; 
+using System.Windows.Controls;
+using System.Windows.Forms.VisualStyles;
 using System.Windows.Input;
-using minifmod4net;
-
 namespace EMArcadeShell.UI
 { 
 	public partial class WpfGamelist : Window
 	{
-		readonly Thread _thread = new Thread(PlayMusic); //знаю, палкой бить.
+		public System.Windows.Forms.NotifyIcon Ni = new System.Windows.Forms.NotifyIcon();
+
 		public WpfGamelist()
 		{ 
 			InitializeComponent();
 			GameListBox.SelectedIndex = 0;
-			GameListBox.Focus(); 
+			GameListBox.Focus();
+			try
+			{
+				Ni.Icon = new System.Drawing.Icon(Directory.GetCurrentDirectory() + "\\CONTENT\\IMG\\" + "fav.ico");
+			}
+			catch (Exception exxs)
+			{
+				Console.WriteLine(exxs.ToString());
+			}
+			Ni.Visible = true;
+			Ni.DoubleClick +=
+				delegate {
+					if (WindowState == WindowState.Normal) return;
+						Topmost = true;
+						Show();//Показываем
+						WindowState = WindowState.Normal;//Вместо триггера.
+					PlayMusic();
+				};
 		}
 
-		private static void PlayMusic()
+		private  void PlayMusic()
 		{
 			try
 			{
-				var xmpath = Directory.GetCurrentDirectory() + "\\CONTENT\\XM\\";
-                var registry = new MiniFmodModuleRegistry();
-
+				var mp3Path = Directory.GetCurrentDirectory() + "\\CONTENT\\MP3\\";
+				WpfMediaMp3Player.Source = new Uri(mp3Path + "NeverGonnaGiveYouUp.mp3");
+				WpfMediaMp3Player.Play();
 				#region RandomTrack
+
 				//получить список файлов из xmpath (там их будет от 1 до 10).
 				//выбрать случайный.
 				//воспроизвести ниже.
+
 				#endregion
-
-				var bytes = File.ReadAllBytes(xmpath + "\\test.xm");
-				var module = registry.LoadFromMemory(1, bytes);
-				module.Play();
-				while (true) ;
-
 
 			}
 			catch (Exception exex)
 			{
-				Console.WriteLine(exex);
+				Console.WriteLine(exex.ToString());
 			}
 		}
 
 		private  void StopMusic()
 		{
-			_thread.Abort();//прерываем поток
-			_thread.Join(500);//таймаут на завершение
-
+			WpfMediaMp3Player.Stop();
 		}
 		 
 		private void WpfGameList(object sender, KeyEventArgs e)
-		{ 
+		{
+			#region DEBUG
 			if (e.Key == Key.Escape)
 			{
 				Close();
 			}
-			if (e.Key == Key.LeftCtrl)
+			if (e.Key == Key.LeftCtrl) //эмуляция события сворачивания по запуску игры.
 			{
 				StopMusic();
-				WindowState = WindowState.Minimized; //эмуляция события сворачивания по запуску игры.
-				
+				Hide(); //Прячем с глаз долой.
+				WindowState = WindowState.Minimized; //Вместо триггера.
+				Topmost = false;
 			}
-			
-			//	AdjustWindowSize(); //эмуляция развертывания после закрытия эмуля
-			
+			#endregion
 		}
 
 		private void GameListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
-			
+			//...
 		}
 
 		private void WpfGamelist_Loaded(object sender, RoutedEventArgs e)
 		{
-			_thread.Start();
-
+			PlayMusic();
 		}
 
-		private void AdjustWindowSize()
-		{
-
-			if (WindowState == WindowState.Minimized)
-			{
-				WindowState = WindowState.Normal;
-				_thread.Start();
-			}
-			else
-			{
-				WindowState = WindowState.Minimized;
-				StopMusic();
-			}
-		}
-
-
+		 
 		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
 		{
-			StopMusic();
+			Ni.Icon = null; //иконок на таскбаре нема
+			StopMusic(); //Останавливаем музыку. На всякий
 		}
 	}
 }
